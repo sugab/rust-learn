18be2390a8c7aeac4806cd3e2f34cef13004db1e
bagus.cahyono <bagus.cahyono@brank.as>
2025-01-13 15:32:51 +0700

    fix: finish primitive types and quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(x: i32) -> i32 {
+    if x > 40 {
+        x
+    } else {
+        x * 2
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
