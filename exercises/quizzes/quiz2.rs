1135601d6843a50bfe0713ff82e50648855fcfdb
bagus.cahyono <baguscah77@gmail.com>
2025-01-16 19:14:24 +0700

    fix: quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,16 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(s, c)| match c {
+                Command::Trim => s.trim().to_string(),
+                Command::Uppercase => s.to_uppercase(),
+                Command::Append(n) => s + "bar".repeat(n).as_str(),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,7 +46,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
